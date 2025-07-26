0df6797eec618428ac02a7ce466107d07dacb47d
dimryb <dimryb@bk.ru>
2025-07-26 23:33:27 +0700

    Добавил утилиты из API MEXC

---
+++ internal/api/mexc/config/config.go
@@ -0,0 +1,7 @@
+package config
+
+const (
+	API_KEY  string = ""                            //nolint: revive, stylecheck
+	SEC_KEY  string = ""                            //nolint: revive, stylecheck
+	BASE_URL string = "https://api.mexc.com/api/v3" //nolint: revive, stylecheck
+)
