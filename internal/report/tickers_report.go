a2caf6580d95bbcfbec01e364978eccf46b13b4e
dimryb <dimryb@bk.ru>
2025-08-01 02:10:33 +0700

    Вынес вывод отчета в отдельный пакет

--- internal/report/tickers_report.go
+++ internal/report/tickers_report.go
@@ -1,1 +1,25 @@
 package report
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dimryb/cross-arb/internal/types"
+)
+
+func PrintTickersReport(results []types.Result) {
+	fmt.Printf("=== Обновление цен (%s) ===\n", time.Now().Format("15:04:05.000"))
+	for _, r := range results {
+		PrintTicker(r.Data)
+	}
+	fmt.Println()
+}
+
+func PrintTicker(t types.BookTicker) {
+	fmt.Printf(
+		"  [%s] -> покупка: %s (%s) | продажа: %s (%s)\n",
+		t.Symbol,
+		t.BidPrice, t.BidQty,
+		t.AskPrice, t.AskQty,
+	)
+}
