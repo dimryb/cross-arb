c486e4b4a24376b154da1d30fda1bdd85fb924a1
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-08-12 14:40:53 +0700

    docs: добавить описание интерфейса TransactionSigner

--- internal/interface/signer.go
+++ internal/interface/signer.go
@@ -2,6 +2,7 @@ package interfaces
 
 import "github.com/gagliardetto/solana-go"
 
+// TransactionSigner абстрагирует подпись транзакции Solana.
 type TransactionSigner interface {
 	PublicKey() solana.PublicKey
 	SignTransaction(tx *solana.Transaction) error
