a7b8b71674fa39f194afaf22d06b3ac9ca120362
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-09-23 21:28:30 +0700

    docs: улучшить документацию mint_pair

--- internal/adapter/jupiter/mint_pair.go
+++ internal/adapter/jupiter/mint_pair.go
@@ -1,7 +1,11 @@
 package jupiter
 
-// MintPair хранит адреса SPL-токенов.
+// MintPair хранит адреса SPL-токенов торговой пары.
 type MintPair struct {
-	InputMint  string
+	// InputMint — адрес BASE-токена в CEX-биржах и INPUT в DEX-биржах.
+	// Например: для пары SOL/USDC это будет SOL
+	InputMint string
+	// OutputMint — адрес QUOTE-токена в CEX-биржах и OUTPUT в DEX-биржах.
+	// Например: для пары SOL/USDC это будет USDC
 	OutputMint string
 }
